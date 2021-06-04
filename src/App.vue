62d4642192022e8f493e4f584013d4919a970321
VindicatorHQ <minecraftgamer725@gmail.com>
2021-06-04 21:07:16 +0200

    Bug push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="content-center">
+      <article>
+        <h1>
+          Wie ben ik?
+        </h1>
+        <p>
+          Mijn naam is Christiaan van Haasteren, ik ben 19 jaar en ben net afgestudeerd van het MBORijnland Leiden. <br>
+          Programmeren is een passie voor mij, ik leer graag veel over nieuwe talen en heb ook heel veel kennis van specifieke talen als ik er eenmaal in zit. <br>
+          Hieronder ga ik dieper in op mijn vooropleidingen, vaardigheden, projecten en kennis.
+        </p>
+      </article>
+      <article class="grid justify-items-stretch">
+        <div>
+          <h1>
+            Mijn vooropleiding
+          </h1>
+          <p>
+
+          </p>
+        </div>
+
+        <div>
+          <h1>
+            Mijn vaardigheden
+          </h1>
+          <p>
+
+          </p>
+        </div>
+
+        <div>
+          <h1>
+            Mijn projecten
+          </h1>
+          <p>
+
+          </p>
+        </div>
+
+        <div>
+          <h1>
+            Kennis Programmeer/Scripttalen
+          </h1>
+          <p>
+
+          </p>
+        </div>
+      </article>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
