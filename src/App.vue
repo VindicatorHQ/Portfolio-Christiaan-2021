4c81db71a3f8eecdb3a04ec7e58eda8067ffd6e5
VindicatorHQ <minecraftgamer725@gmail.com>
2021-06-04 22:26:02 +0200

    Started working on the styling and filling in the information

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
-    <section class="content-center">
-      <article>
-        <h1>
-          Wie ben ik?
-        </h1>
-        <p>
-          Mijn naam is Christiaan van Haasteren, ik ben 19 jaar en ben net afgestudeerd van het MBORijnland Leiden. <br>
-          Programmeren is een passie voor mij, ik leer graag veel over nieuwe talen en heb ook heel veel kennis van specifieke talen als ik er eenmaal in zit. <br>
-          Hieronder ga ik dieper in op mijn vooropleidingen, vaardigheden, projecten en kennis.
-        </p>
+    <section class="grid justify-center">
+      <article class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+        <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="./images/christiaan.jpg" alt="Christiaan">
+        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <h1 class="text-lg font-semibold">
+            Wie ben ik?
+          </h1>
+            <p class="text-lg font-semibold">
+              Mijn naam is Christiaan van Haasteren, ik ben 19 jaar en ben net afgestudeerd van het MBORijnland Leiden. <br>
+              Programmeren is een passie voor mij, ik leer graag veel over nieuwe talen en heb ook heel veel kennis van specifieke talen als ik er eenmaal in zit. <br>
+              Hieronder ga ik dieper in op mijn vooropleidingen, vaardigheden, projecten en kennis.
+            </p>
+        </div>
       </article>
-      <article class="grid justify-items-stretch">
+
+      <article class="grid grid-flow-col grid-rows-2 grid-cols-2 gap-4 text-center">
         <div>
           <h1>
             Mijn vooropleiding
           </h1>
           <p>
-
+            Ik heb een 4 jarige ICT opleiding gevolgd op de Francois Vatel School in Mariahoeve waarbij ik de volgende ervaringen op heb gedaan: <br>
+            Office programma's Word, Excel, Access en Powerpoint en Adobe Photoshop, Premiere Pro, Illustrator.
+            <br><br>
+            Ik heb een 3 jarige Applicatie en Mediaontwikkelaar opleiding gevolgd op het MBORijnland in Leiden. <br>
+            Hier heb ik de basis geleerd van websites ontwikkelen in HTML, CSS, JS, PHP, MySQL en Laravel.
           </p>
         </div>
 
         <div>
-          <h1>
+          <h1 class="text-center">
             Mijn vaardigheden
           </h1>
           <p>
@@ -30,7 +38,7 @@
         </div>
 
         <div>
-          <h1>
+          <h1 class="text-center">
             Mijn projecten
           </h1>
           <p>
@@ -39,7 +47,7 @@
         </div>
 
         <div>
-          <h1>
+          <h1 class="text-center">
             Kennis Programmeer/Scripttalen
           </h1>
           <p>
@@ -47,6 +55,23 @@
           </p>
         </div>
       </article>
+<!--      <div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4">-->
+<!--        <div>-->
+<!--          1-->
+<!--        </div>-->
+<!--        <div class="col-start-3">-->
+<!--          2-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          3-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          4-->
+<!--        </div>-->
+<!--        <div class="row-start-1 col-start-2 col-span-2">-->
+<!--          5-->
+<!--        </div>-->
+<!--      </div>-->
     </section>
 </template>
 
