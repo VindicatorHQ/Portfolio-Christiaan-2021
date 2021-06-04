314a0acacb701f86f833f1166669a24f6d164d64
CrazyVito11 <31567466+CrazyVito11@users.noreply.github.com>
2021-06-04 21:33:01 +0200

    Fixed packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue';
+import Vue from 'vue'
 import App from './App.vue';
 import './css/styles.css'
 
-createApp(App).mount('#app');
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
